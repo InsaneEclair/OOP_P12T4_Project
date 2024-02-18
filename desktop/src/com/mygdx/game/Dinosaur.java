d9c215d529a42f86edd2ab6e34df6cdfaaab8129
lionelchua <lionelchua23@gmail.com>
2024-02-19 01:23:29 +0800

    Added sound when jumping

--- desktop/src/com/mygdx/game/Dinosaur.java
+++ desktop/src/com/mygdx/game/Dinosaur.java
@@ -1,8 +1,10 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.audio.Sound;
 
 public class Dinosaur {
     public Texture texture;
@@ -11,12 +13,15 @@ public class Dinosaur {
     private static final int GRAVITY = -50;
     private static final int MOVEMENT = 100;
     public Rectangle bounds;
+    public Sound jumpSound;
+
 
     public Dinosaur(int x, int y) {
         position = new Vector2(x, y);
         velocity = new Vector2(0, 0);
         texture = new Texture("main-character1.png"); // Make sure you have this texture in your assets
         bounds = new Rectangle(x, y, texture.getWidth(), texture.getHeight());
+        jumpSound = Gdx.audio.newSound(Gdx.files.internal("jump1.wav"));
     }
 
     public void update(float dt) {
@@ -40,6 +45,7 @@ public class Dinosaur {
     public void jump() {
         if (position.y == 0) { // Simple check to prevent double jumping
             velocity.y = 100; // Adjust the jump height as needed
+            jumpSound.play();
         }
     }
 
