847bdb25519feff345e4e4e8eff140efbf0b1f3f
rachelfoong <rachelfoong.26@gmail.com>
2024-02-19 19:19:36 +0800

    added GameStates

--- desktop/src/com/mygdx/game/GameEngine.java
+++ desktop/src/com/mygdx/game/GameEngine.java
@@ -4,21 +4,74 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.Game;
 
 public class GameEngine extends Game {
-	public SpriteBatch batch;
+	private SpriteBatch batch;
+    private boolean gameStarted;
+    private GameState gameState;
+    public enum GameState {
+        RUNNING,
+        PAUSED,
+        GAME_OVER
+    }
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        this.setScreen(new GameScreen(this));
+        gameStarted = false;
+        gameState = GameState.RUNNING;
+    }
+
+    public void start() {
+        this.setScreen(new GameScreen(this, batch));
+        gameStarted = true;
+    }
+
+    public void pause() {
+        gameState = GameState.PAUSED;
+    }
+
+    public void resume() {
+        gameState = GameState.RUNNING;
+    }
+
+    public void restart() {
+        // reset game variables
+        gameStarted = false;
+        gameState = GameState.RUNNING;
+
+        // create a new GameScreen instance
+        GameScreen newScreen = new GameScreen(this, batch);
+
+        // set the new screen as the current screen
+        this.setScreen(newScreen);
+    }
+
+    public void end() {
+        gameState = GameState.GAME_OVER;
+        dispose();
     }
 
     @Override
     public void render() {
+        if (!gameStarted) {
+            start();
+        }
         super.render(); // This will render the current screen
     }
 
     @Override
     public void dispose() {
         batch.dispose();
+    }
+
+    public boolean isGameStarted() {
+        return gameStarted;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public void setGameState(GameState state) {
+        gameState = state;
     }
 }
--- desktop/src/com/mygdx/game/GameScreen.java
+++ desktop/src/com/mygdx/game/GameScreen.java
@@ -6,15 +6,18 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Random;
 
 public class GameScreen implements Screen {
 	private static final int MOVEMENT = 100;
-	final GameEngine game;
-    OrthographicCamera camera;
-    Dinosaur dinosaur;
+	private final GameEngine game;
+    private final SpriteBatch batch;
+    private OrthographicCamera camera;
+    private Dinosaur dinosaur;
     private Texture[] backgrounds;
     private float[] backgroundOffset;
     private ArrayList<EntityManager> obstacles = new ArrayList<EntityManager>();
@@ -27,8 +30,9 @@ public class GameScreen implements Screen {
     private static final float groundYPosition = 0;
     private static final float airYPosition = 25;
 
-    public GameScreen(final GameEngine game) {
+    public GameScreen(final GameEngine game, SpriteBatch batch) {
         this.game = game;
+        this.batch = batch;
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
@@ -52,69 +56,71 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        if (game.isGameStarted() && game.getGameState() != GameEngine.GameState.GAME_OVER) {
+            Gdx.gl.glClearColor(0, 0, 0, 1);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
-            dinosaur.jump();
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            dinosaur.duck();
-        } else {
-            dinosaur.standUp();
-        }
-
-        dinosaur.update(delta);
-        camera.update();
-        game.batch.setProjectionMatrix(camera.combined);
-        
-        game.batch.begin();
-        float scaleFactor = camera.viewportWidth / (backgrounds[0].getWidth());
-        float scaledHeight = backgrounds[0].getHeight() * (2*scaleFactor);
-
-        // Draw and update backgrounds
-        for (int i = 0; i < backgrounds.length; i++) {
-            if (backgroundOffset[i] + backgrounds[i].getWidth() * scaleFactor <= 0) {
-                backgroundOffset[i] += backgrounds[i].getWidth() * scaleFactor * backgrounds.length;
+            if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
+                dinosaur.jump();
             }
-            game.batch.draw(backgrounds[i], backgroundOffset[i], 0, backgrounds[i].getWidth() * scaleFactor, scaledHeight);
-            backgroundOffset[i] -= MOVEMENT * delta;
-        }
-
-        // Manage obstacles
-        timeSinceLastObstacle += delta;
-        timeUntilNextObstacle -= delta;
-        if (timeUntilNextObstacle <= 0) {
-            EntityManager newObstacle;
-            int obstacleType = random.nextInt(obstacleTextures.length + 1); // Assuming you have 2 cactus types + cloud
-            if (obstacleType < obstacleTextures.length) {
-                // Create a cactus obstacle
-                String textureName = obstacleTextures[obstacleType];
-                newObstacle = new Cactus(camera.viewportWidth, groundYPosition, textureName);
+            if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+                dinosaur.duck();
             } else {
-                // Create a cloud obstacle
-                newObstacle = new Cloud(camera.viewportWidth, airYPosition);
+                dinosaur.standUp();
             }
-            obstacles.add(newObstacle);
-            timeSinceLastObstacle = 0;
-            timeUntilNextObstacle = getRandomSpawnTime();
-        }
 
-        Iterator<EntityManager> iter = obstacles.iterator();
-        while (iter.hasNext()) {
-            EntityManager obstacle = iter.next();
-            obstacle.update(delta);
-            if (obstacle.position.x + obstacle.texture.getWidth() < 0) {
-                iter.remove(); // Remove obstacle if it has passed off the screen
-                obstacle.dispose();
-            } else {
-                game.batch.draw(obstacle.texture, obstacle.position.x, obstacle.position.y);
+            dinosaur.update(delta);
+            camera.update();
+            batch.setProjectionMatrix(camera.combined);
+
+            batch.begin();
+            float scaleFactor = camera.viewportWidth / (backgrounds[0].getWidth());
+            float scaledHeight = backgrounds[0].getHeight() * (2*scaleFactor);
+
+            // Draw and update backgrounds
+            for (int i = 0; i < backgrounds.length; i++) {
+                if (backgroundOffset[i] + backgrounds[i].getWidth() * scaleFactor <= 0) {
+                    backgroundOffset[i] += backgrounds[i].getWidth() * scaleFactor * backgrounds.length;
+                }
+                batch.draw(backgrounds[i], backgroundOffset[i], 0, backgrounds[i].getWidth() * scaleFactor, scaledHeight);
+                backgroundOffset[i] -= MOVEMENT * delta;
             }
-        }
-        game.batch.draw(dinosaur.getTexture(), dinosaur.position.x, dinosaur.position.y);
-        game.batch.end();
 
-        Gdx.app.log("Dinosaur Position", dinosaur.position.toString());
+            // Manage obstacles
+            timeSinceLastObstacle += delta;
+            timeUntilNextObstacle -= delta;
+            if (timeUntilNextObstacle <= 0) {
+                EntityManager newObstacle;
+                int obstacleType = random.nextInt(obstacleTextures.length + 1); // Assuming you have 2 cactus types + cloud
+                if (obstacleType < obstacleTextures.length) {
+                    // Create a cactus obstacle
+                    String textureName = obstacleTextures[obstacleType];
+                    newObstacle = new Cactus(camera.viewportWidth, groundYPosition, textureName);
+                } else {
+                    // Create a cloud obstacle
+                    newObstacle = new Cloud(camera.viewportWidth, airYPosition);
+                }
+                obstacles.add(newObstacle);
+                timeSinceLastObstacle = 0;
+                timeUntilNextObstacle = getRandomSpawnTime();
+            }
+
+            Iterator<EntityManager> iter = obstacles.iterator();
+            while (iter.hasNext()) {
+                EntityManager obstacle = iter.next();
+                obstacle.update(delta);
+                if (obstacle.position.x + obstacle.texture.getWidth() < 0) {
+                    iter.remove(); // Remove obstacle if it has passed off the screen
+                    obstacle.dispose();
+                } else {
+                    batch.draw(obstacle.texture, obstacle.position.x, obstacle.position.y);
+                }
+            }
+            batch.draw(dinosaur.getTexture(), dinosaur.position.x, dinosaur.position.y);
+            batch.end();
+
+            Gdx.app.log("Dinosaur Position", dinosaur.position.toString());
+        }
     }
 
     private float getRandomSpawnTime() {
@@ -157,11 +163,22 @@ public class GameScreen implements Screen {
     @Override
     public void pause() {
         // Implement pause logic
+        game.pause();
     }
 
     @Override
     public void resume() {
         // Implement resume logic
+        game.resume();
+    }
+
+    public void restart() {
+        game.restart();
+    }
+
+    public void end() {
+        dispose();
+        game.end();
     }
 
     @Override
