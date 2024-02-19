a3390015f239dfa169137cfe425979c512fbba54
TSL <soon19982013@hotmail.com>
2024-02-19 17:52:57 +0800

    renamed Obstacle.java name to EntityManager.java Added Cloud and Cactus Obstacles. Added object spawning logic in GameScreen.java

---
+++ desktop/src/com/mygdx/game/EntityManager.java
@@ -0,0 +1,28 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+
+public class EntityManager {
+    private static final int MOVEMENT = -100;
+    public Texture texture;
+    public Vector2 position;
+    public Rectangle bounds;
+
+    public EntityManager(float x, float y, String textureFileName) {
+        texture = new Texture(Gdx.files.internal(textureFileName));
+        position = new Vector2(x, y);
+        bounds = new Rectangle(x, y, texture.getWidth(), texture.getHeight());
+    }
+
+    public void update(float dt) {
+        position.add(MOVEMENT * dt, 0);
+        bounds.setPosition(position.x, position.y);
+    }
+
+    public void dispose() {
+        texture.dispose();
+    }
+}
