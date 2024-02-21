505910c58873d2179b55f191c9144ce68c16d49f
TSL <soon19982013@hotmail.com>
2024-02-21 18:28:20 +0800

    Removed Commented out code and added speed modifier. Removed position.add and bounds.setposition as they are unnecessary since collision is based off EntityManager.java update().

--- desktop/src/com/mygdx/game/Balloon.java
+++ desktop/src/com/mygdx/game/Balloon.java
@@ -1,25 +1,11 @@
 // Balloon.java
 package com.mygdx.game;
 
-import com.badlogic.gdx.math.Vector2;
-
 public class Balloon extends EntityManager {
 
-
-    public Balloon(float x, float y) {
-        super(x, 45, "balloon.jpg");
+    public Balloon(float x, float y, float obstacleSpeed) {
+        super(x, y, "balloon.jpg");
+        MOVEMENT = (int) obstacleSpeed; // Ensure MOVEMENT is not final and can be set
     }
-
-    // Adjust the position of the balloon based on the dinosaur's position
-    public void update(float dt) {
-//        if (isDucking) {
-//            position.set(dinoPosition.x + BALLOON_OFFSET_X, dinoPosition.y + BALLOON_OFFSET_Y);
-//            bounds.setPosition(position.x, position.y);
-//        } else {
-//            super.update(dt);
-//        }
-        position.add(MOVEMENT * dt, 0);
-        bounds.setPosition(position.x, position.y);
-    }
-    }
+}
 
