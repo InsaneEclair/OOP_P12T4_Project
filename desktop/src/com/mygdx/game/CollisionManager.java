99dff2bfde35ec0b7f919402fe19d1efb765f359
TSL <soon19982013@hotmail.com>
2024-02-21 18:29:09 +0800

    Replaced Cacti/Cactus to Obstacles as it is the accurate representation of what is happening.

--- desktop/src/com/mygdx/game/CollisionManager.java
+++ desktop/src/com/mygdx/game/CollisionManager.java
@@ -1,27 +1,26 @@
 package com.mygdx.game;
 import java.util.ArrayList;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.math.Rectangle;
 
 public class CollisionManager {
     private Dinosaur dinosaur;
-    private ArrayList<EntityManager> cacti;
+    private ArrayList<EntityManager> obstacle;
     private GameEngine gameEngine; // This should be GameEngine, not GameScreen
 
-    public CollisionManager(Dinosaur dinosaur, ArrayList<EntityManager> cacti, GameEngine gameEngine) {
+    public CollisionManager(Dinosaur dinosaur, ArrayList<EntityManager> obstacle, GameEngine gameEngine) {
         this.dinosaur = dinosaur;
-        this.cacti = cacti;
+        this.obstacle = obstacle;
         this.gameEngine = gameEngine; // Store the reference to GameEngine
     }
 
     public boolean checkCollision() {
         Rectangle dinosaurBounds = dinosaur.getBounds();
 
-        for (EntityManager cactus : cacti) {
-            Rectangle cactusBounds = cactus.getBounds();
+        for (EntityManager obstacles : obstacle) {
+            Rectangle obstaclesBounds = obstacles.getBounds();
 
-            if (dinosaurBounds.overlaps(cactusBounds)) {
+            if (dinosaurBounds.overlaps(obstaclesBounds)) {
                 gameEngine.end(); // Use the game engine to transition to the GameOverScreen
                 return true; // Collision occurred
             }
