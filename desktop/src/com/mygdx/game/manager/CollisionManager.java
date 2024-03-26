754011d3c8ad506af6133b22991eb895e0a38d74
InsaneEclair <soon19982013@hotmail.com>
2024-03-27 00:51:42 +0800

    Change from "GameObject or gameObject" to "Entity or entity"

--- desktop/src/com/mygdx/game/manager/CollisionManager.java
+++ desktop/src/com/mygdx/game/manager/CollisionManager.java
@@ -5,18 +5,18 @@ import java.util.Iterator;
 import com.badlogic.gdx.math.Rectangle;
 import com.mygdx.game.GameEngine;
 import com.mygdx.game.entity.Dinosaur;
-import com.mygdx.game.entity.GameObject;
+import com.mygdx.game.entity.Entity;
 import com.mygdx.game.entity.Star;
 
 public class CollisionManager {
     private final Dinosaur dinosaur;
-    private final ArrayList<GameObject> gameObjects; // Renamed for clarity
+    private final ArrayList<Entity> entities; // Renamed for clarity
     private final GameEngine gameEngine;
     private final SoundManager soundManager;
 
-    public CollisionManager(Dinosaur dinosaur, ArrayList<GameObject> gameObjects, GameEngine gameEngine) {
+    public CollisionManager(Dinosaur dinosaur, ArrayList<Entity> entities, GameEngine gameEngine) {
         this.dinosaur = dinosaur;
-        this.gameObjects = gameObjects;
+        this.entities = entities;
         this.gameEngine = gameEngine;
         this.soundManager = new SoundManager();
     }
@@ -25,14 +25,14 @@ public class CollisionManager {
         Rectangle dinosaurBounds = dinosaur.getBounds();
 
         // Use an iterator to safely remove elements from the list while iterating
-        Iterator<GameObject> iterator = gameObjects.iterator();
+        Iterator<Entity> iterator = entities.iterator();
         while (iterator.hasNext()) {
-            GameObject gameObject = iterator.next();
-            Rectangle gameObjectBounds = gameObject.getBounds();
+            Entity entity = iterator.next();
+            Rectangle gameObjectBounds = entity.getBounds();
 
             if (dinosaurBounds.overlaps(gameObjectBounds)) {
-                if (gameObject instanceof Star) {
-                        Star star = (Star) gameObject;
+                if (entity instanceof Star) {
+                        Star star = (Star) entity;
                         String textureFileName = star.getTextureFileName();
                         if(textureFileName.equals("entity/smallstar.png")) {
                             gameEngine.incrementScore(100);
