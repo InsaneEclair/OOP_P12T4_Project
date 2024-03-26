83e43c223c69ceea390df5bc210e83c2d9ed6f66
InsaneEclair <soon19982013@hotmail.com>
2024-03-27 01:01:44 +0800

    Created EntityManger to control Entities.

--- desktop/src/com/mygdx/game/manager/EntityManager.java
+++ desktop/src/com/mygdx/game/manager/EntityManager.java
@@ -1,6 +1,5 @@
 package com.mygdx.game.manager;
 
-import com.mygdx.game.entity.Dinosaur;
 import com.mygdx.game.entity.Entity;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,34 +9,15 @@ import static com.badlogic.gdx.math.MathUtils.random;
 
 public class EntityManager {
     private final List<Entity> entities = new ArrayList<>();
-    private AIManager aiManager;
 
     public void addEntity(Entity entity) {
         entities.add(entity);
-    }
-    public void removeEntity(Entity entity) {
-        entities.remove(entity);
     }
 
     public void update(float delta) {
         for (Entity entity : new ArrayList<>(entities)) {
             entity.update(delta);
         }
-    }
-
-    public void spawnEntity(AIManager.GameObjectType type, float x, float y, float speed) {
-        // Use GameObjectFactory to create the GameObject
-        Entity newEntity = GameObjectFactory.createEntity(type, x, y, speed); // Adjusted method name
-        addEntity(newEntity); // Add the new entity to the list of managed entities
-    }
-
-    public Dinosaur getDinosaur() {
-        for (Entity entity : entities) {
-            if (entity instanceof Dinosaur) {
-                return (Dinosaur) entity;
-            }
-        }
-        return null; // Or create and return the Dinosaur instance
     }
 
     public void spawnGameObject(AIManager.GameObjectType type, GameScreen gameScreen, float gameObjectSpeed) {
