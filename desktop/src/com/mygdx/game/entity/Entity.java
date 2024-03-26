4c332b93900be8b152352d18d15f15cff70ab80d
InsaneEclair <soon19982013@hotmail.com>
2024-03-27 00:50:10 +0800

    Change "GameObject" to "Entity", new Functions, setMovement and getMovement. cleaned up code

--- desktop/src/com/mygdx/game/entity/Entity.java
+++ desktop/src/com/mygdx/game/entity/Entity.java
@@ -5,30 +5,28 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
-public class GameObject {
-    private static int MOVEMENT = -150;
+public abstract class Entity {
+    private int movement = -150;
     private final Texture texture;
     private final Vector2 position;
     private final Rectangle bounds;
     private float speed;
 
-    public GameObject(float x, float y, String textureFileName) {
+    public Entity(float x, float y, String textureFileName) {
         texture = new Texture(Gdx.files.internal(textureFileName));
         position = new Vector2(x, y);
         bounds = new Rectangle(x, y, texture.getWidth(), texture.getHeight());
     }
 
     public void update(float dt) {
-        position.add(MOVEMENT * dt, 0);
+        position.add(getMovement() * dt, 0);
         bounds.setPosition(position.x, position.y);
     }
 
+    public void setMovement(int movement) {this.movement = movement;}
+
     public Rectangle getBounds() {
         return bounds;
-    }
-
-    public static void setMOVEMENT(int MOVEMENT) {
-        GameObject.MOVEMENT = MOVEMENT;
     }
 
     public Texture getTexture() {
@@ -39,16 +37,15 @@ public class GameObject {
         return position;
     }
 
-    public float getSpeed() {
-        return speed;
-    }
+    public float getSpeed() {return speed;}
 
-    public void setSpeed(float speed) {
-        this.speed = speed;
-    }
+    public void setSpeed(float speed) {this.speed = speed;}
 
     public void dispose() {
         texture.dispose();
     }
 
+    public int getMovement() {
+        return movement;
+    }
 }
